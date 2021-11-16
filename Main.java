44c0ea3684361e2f11e3b66a4cd25bd7f34f2545
cannpg <80770043+Cannpg@users.noreply.github.com>
2021-11-16 19:45:16 +0000

    x

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ class Main {
   public static void main(String[] args) {
     
     ArrayList<Person> al = new ArrayList<Person>();
+    ArrayList<Person> people = new ArrayList<Person>();
     
     Person p1 = new Person("Parker", 21, "Vermillion");
     Person p2 = new Person("A", 87, "Pink");
@@ -20,18 +21,48 @@ class Main {
 
     try{
         FileWriter myFile;
-        myFile = new FileWriter"questions.txt");
-        BufferedWriter reader = new BufferedWriter(myFile);
+        myFile = new FileWriter("questions.txt");
+        BufferedWriter writer = new BufferedWriter(myFile);
 
-        while (reader.ready()) 
-          for(int i = 0; i<al.size(); i++){
-            reader.writeLine(al.get(i).getName());
-          }
+        for(int i = 0; i<al.size(); i++){
+            writer.write(al.get(i).getName());
+          writer.newLine();
+            writer.write(Integer.toString(al.get(i).getAge()));
+          writer.newLine();
+            writer.write(al.get(i).getColor());
+          writer.newLine();
+        }
+
+      writer.flush();
+      }
+    catch (IOException exception) {
+      System.out.println("An error occurred: " + exception);
+    }
+
+    try{
+        FileReader myFile;
+        myFile = new FileReader("data.txt");
+        BufferedReader reader = new BufferedReader(myFile);
+
+        while (reader.ready()) {
+          String name = reader.readLine();
+          int age = Integer.parseInt(reader.readLine());
+          String color = reader.readLine();
+
+          Person p = new Person(name, age, color);
+
+          people.add(p);
+        }
 
         reader.close();
       }
     catch (IOException exception) {
       System.out.println("An error occurred: " + exception);
+    }
+
+    for(int i = 0; i < people.size(); i++){
+      Person object = people.get(i);
+      System.out.printf("'%-10s' %n", object.name, object.age, object.color);
     }
   }
 }
